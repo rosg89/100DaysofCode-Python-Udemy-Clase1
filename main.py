1559433c0ca58f2f24786922cc04835454913d88
Ro <rsuarezgalan@hotmail.com>
2022-03-30 12:53:20 -0300

    Se sube el proyecto

---
+++ main.py
@@ -0,0 +1,21 @@
+#Salto de línea
+print("Hello World!\nHello World!\nHello World!")
+
+#Concatenar
+print("Good" + " " + "Morning")
+
+#Input
+#input("")
+print("Hello " + input("What is your nickname? "))
+
+#Len / Cantidad de caracteres
+print(len(input("Where are you from? ")))
+
+#Variables
+name = input("What is your name? ")
+surname = input("What is your surname? ")
+print(name+ " "+surname)
+
+#Se separa con _
+user_name = input("What is your username? ")
+print(user_name)
